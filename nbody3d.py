7cc5acc162082416caa60929143664490685a028
wingos80 <wingyc80@gmail.com>
2022-01-03 19:01:26 +0100

    added circular orbit initialization and changed frame time printing code

--- nbody3d.py
+++ nbody3d.py
@@ -4,6 +4,7 @@ import numpy as np
 import moderngl, moderngl_window as mglw
 import shaders
 
+
 class ComputeParticleBase(mglw.WindowConfig):
     gl_version = 4, 3  # Required opengl version
     window_size = 800, 800  # Initial window size
@@ -13,31 +14,42 @@ class ComputeParticleBase(mglw.WindowConfig):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-        self.n_bodies = 512
-        m = 1
-        m_sun = 10000000
+        self.n_bodies = 2048
+        m = 0.00000001
+        m_sun = np.power(10,7)  # Mass of sun, roughly 10^6 times bigger than earth
         # Create the two buffers the compute shader will write and read from
-        #initial_state = np.genfromtxt("init.csv", skip_header=1, delimiter=",")[:,1:].astype("f4")
+        array = np.genfromtxt("init.csv", skip_header=1, delimiter=",")[:,1:].astype("f4")
         #initial_state = initial_state[:, [2,3,4,1,5,6,7,0,8,9,10,11]] # Re-order columns to match layout in shader
         initial_state = np.random.uniform(-0.95, 0.95, (self.n_bodies,12)).astype("f4")
+        # initial_state = np.ones((self.n_bodies, 12)).astype("f4")
         initial_state[:,2] = 0
-        initial_state[:,3] = 0.02 # Radius
-        initial_state[:,7] = np.random.uniform(1, 1000, (self.n_bodies)).astype('f4') # Mass
-        # initial_state[:,4:7] = 0 # Zero velocity
+        initial_state[:,3] = 0.005 # Radius
+        initial_state[:,7] = m # Mass
         initial_state[:,8:11] = np.random.uniform(0.5, 1.0, (self.n_bodies,3)).astype("f4") # Color
         initial_state[:,11] = 1 # Alpha
+
+        ## Primitively generating Mars and Earth
+        initial_state[1:3, 1] = 0   # Setting y coord of both mars n earth to be 0
+        initial_state[1, 0] = 0.5   # x coord of earth, 0.5 in game distance equals 1 AU
+        initial_state[2, 0] = 0.75  # x coord of mars, 1.5 AU
+        initial_state[1, 7] = 10    # Mass of earth
+        initial_state[2, 7] = 1     # Mass of mars, is roughly 10 times smaller than earth
+        initial_state[1:3, 3] = 0.02# Making radius of earth and mars bigger so its more visible
+        initial_state[1:3, 8:11] = np.array([1,1,1])
+
+        ## Generating circular orbits for all the bodies
         pos = initial_state[:,0:3]
         r = np.sqrt(np.sum(pos**2,axis=1))
-
-        v = np.sqrt((m_sun * m) / r)
+        v = np.sqrt((m_sun) / r)
         vx = -v*pos[:,1]/r
         vy = v*pos[:,0]/r
         vz = v*pos[:,2]/r
         initial_state[:,4:7] = np.array([vx, vy, vz]).transpose()/1
 
+
         ## Creating the Sun
         initial_state[0, :3] = 0  # Pos
-        initial_state[0, 3] = 0.04  # Radius
+        initial_state[0, 3] = 0.045  # Radius
         initial_state[0,4:7] = 0 # Velocity
         initial_state[0, 7] = m_sun  # Mass
         initial_state[0, 8:11] = np.array([1,0,0])  # Color
@@ -68,10 +80,21 @@ class ComputeParticleBase(mglw.WindowConfig):
         self.vao_particles = self.ctx.vertex_array(
             self.program, [(self.buf_particles, '4f 4x4 4f', 'in_vert', 'in_col')],
         )
-    def render(self, time, frame_time):
-        if frame_time > 0.0001: print(f"Frame time: {frame_time * 1000} ms")
 
-        dt = 0.00001
+        ## Frame time stuff
+        self.frame_times = [0,0]
+
+
+    def render(self, time, frame_time):
+        # if frame_time > 0.0001: print(f"Frame time: {frame_time * 1000} ms")
+        self.frame_times[0] += frame_time
+        self.frame_times[1] += 1
+
+        if self.frame_times[1] >= 50:
+            print(f'Frame time = {round(self.frame_times[0] / self.frame_times[1] * 1000, 2)}ms')
+            self.frame_times[0], self.frame_times[1] = 0, 0
+
+        dt = 0.0002
         # Bind buffers
         self.buf_particles.bind_to_storage_buffer(0)
         self.buf_acc.bind_to_storage_buffer(1)
